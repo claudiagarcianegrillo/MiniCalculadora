14f55807d8de3605e2ae802939b96527fc726cc7
claudiagarcianegrillo <claudiagarcianegrillo@gmail.com>
2026-04-09 12:06:02 +0200

    d.raiz

--- src/main/java/MiniCalculadora.java
+++ src/main/java/MiniCalculadora.java
@@ -1,4 +1,22 @@
+/**
+ * Clase que nos proporciona operaciones matemáticas como son
+ * la raíz cuadrada y el valor absoluto
+ *
+ * @author Claudia Garcia
+ * @version 1.0
+ * @since 1.0
+ */
+
 public class MiniCalculadora {
+
+    /**
+     * Calcula la raíz cuadrada de un número
+     *
+     * @param numero Número del cual se desea obtener la raíz cuadrada
+     * @return Nos devuelve la raíz cuadrada del número que hayamos introducido
+     * @see Math#sqrt(double)
+     * @throws IllegalArgumentException Por si el número es negativo
+     */
 
     public double raizCuadrada(double numero) {
         return Math.sqrt(numero);
